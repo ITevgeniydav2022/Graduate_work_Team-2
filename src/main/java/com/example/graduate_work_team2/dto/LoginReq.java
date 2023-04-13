c5163e6fb3cfa0e28dfb4a98bfd0f6f8341c080c
ITevgeniydav2022 <defox2022@gmail.com>
2023-04-13 14:12:01 +0300

    Commit one.

---
+++ src/main/java/com/example/graduate_work_team2/dto/LoginReq.java
@@ -0,0 +1,11 @@
+package com.example.graduate_work_team2.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginReq {
+    private String password;
+    private String username;
+
+}
+
---
+++ src/main/java/com/example/graduate_work_team2/dto/Role.java
@@ -0,0 +1,5 @@
+package com.example.graduate_work_team2.dto;
+
+public enum Role {
+    USER, ADMIN
+}
